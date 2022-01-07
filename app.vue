77fe929b0ed3c5d509920b629fa323f713129386
Jveen-D <d85066965@icloud.com>
2022-01-07 16:26:44 +0800

    chore:基础构建

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<!--
+ * @Date: 2021-11-23 07:13:18
+ * @LastEditors: dwj18066042960
+ * @FilePath: /Nuxt-Blog/app.vue
+-->
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage/>
   </div>
 </template>
---
+++ pages/home.vue
@@ -0,0 +1,10 @@
+<!--
+ * @Date: 2022-01-07 16:18:15
+ * @LastEditors: dwj18066042960
+ * @FilePath: /Nuxt-Blog/pages/home.vue
+-->
+<template>
+  <div>
+    home
+  </div>
+</template>
